9eb6d11d7a482ae5c87fdbbc6d767c3f7f823f53
MobilisInMobile <58860013+MobilisInMobile@users.noreply.github.com>
2019-12-20 18:24:20 +0200

    Причесал для ЗдфеащьШЩ

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+volatile boolean flag;
+volatile unsigned long myTimer;
+void myInterrupt();
+
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    
+    // Use D2 in pinmode and attach
+    pinMode(2, INPUT_PULLUP);
+    attachInterrupt(0, myInterrupt, FALLING);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    if (flag)
+    {
+        Serial.print("INTERRUPT!!!");
+        Serial.println(myTimer);
+        flag = 0;
+    }
+}
+void myInterrupt()
+{
+    flag = 1;
+    myTimer = millis() ;
 }
